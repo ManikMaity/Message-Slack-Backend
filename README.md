747f53e6d9407217202eaffd9e64165ad7d3642c
Manik Maity <manikmaity156@gmail.com>
2024-11-15 10:32:18 +0530

    Added db config

--- README.md
+++ README.md
@@ -6,3 +6,5 @@
 - `eslint` - eslint is a tool for identifying and reporting on patterns found in JavaScript. [Atricle to Setup](https://medium.com/@sindhujad6/setting-up-eslint-and-prettier-in-a-node-js-project-f2577ee2126f).
 - `prettier` - prettier is a code formatter.
 - `"simple-import-sort` - This is a pulugin for eslint to sort imports.
+- In production we have two environments, development and production.
+- So we can setup our database according to the environment in db.config.js.
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import statusCodes from 'http-status-codes'
 
+import connectDB from './config/db.config.js'
 import { PORT } from './config/variables.js'
 
 const app = express()
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/ping', (req, res) => {
   res.status(statusCodes.OK).json({
@@ -12,6 +14,8 @@ app.get('/ping', (req, res) => {
     message: 'pong'
   })
 })
+
+await connectDB()
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`)
