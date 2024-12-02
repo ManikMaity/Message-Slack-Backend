00ef9824c3e2de2dd69a13acef2179fa6a92cc95
Manik Maity <manikmaity156@gmail.com>
2024-12-02 17:35:23 +0530

    HW done - Message to specific user done

--- src/controllers/messageSocket.controller.js
+++ src/controllers/messageSocket.controller.js
@@ -1,7 +1,24 @@
 import { createMessageService } from "../services/message.service.js";
-import { NEW_MESSAGE_EVENT, NEW_MESSAGE_RECEIVED } from "../utils/common/socketEventConstant.js";
+import { NEW_CHANNEL_MESSAGE, NEW_CHANNEL_MESSAGE_RECEIVED, NEW_MESSAGE_EVENT, NEW_MESSAGE_RECEIVED } from "../utils/common/socketEventConstant.js";
 
 export function messageHandler(io, socket) {
+
+  const roomId = socket.handshake.query.roomId;
+  if (roomId) {
+    socket.join(roomId);
+    console.log('Joined room', roomId);
+  }
+
+  socket.on(NEW_CHANNEL_MESSAGE, async function createChannelMessage(data, cb) {
+    const messageResponse = await createMessageService({...data, channelId: roomId});
+    socket.to(roomId).emit(NEW_CHANNEL_MESSAGE_RECEIVED, messageResponse);
+    cb({
+      success: true,
+      message: 'Message created successfully',
+      data: messageResponse
+    })
+  })
+
   socket.on(NEW_MESSAGE_EVENT, async function createMessageHandler(data, cb){
     const messageResponse = await createMessageService(data);
     socket.broadcast.emit(NEW_MESSAGE_RECEIVED, messageResponse);
--- src/utils/common/socketEventConstant.js
+++ src/utils/common/socketEventConstant.js
@@ -1,2 +1,4 @@
 export const NEW_MESSAGE_EVENT = "NewMessage";
 export const NEW_MESSAGE_RECEIVED = "NewMessageReceived";
+export const NEW_CHANNEL_MESSAGE = "NewChannelMessage";
+export const NEW_CHANNEL_MESSAGE_RECEIVED = "NewChannelMessageReceived";
